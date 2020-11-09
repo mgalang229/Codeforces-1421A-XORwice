47c2cc7230c6596bea2241e33a8c13da0b9a0cf3
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-11-09 09:59:34 +0800

    Create sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int t;
+	cin >> t;
+	while(t--) {
+		int a, b;
+		cin >> a >> b;
+		if(a>b)
+			swap(a, b);
+		cout << (int)((a^a)+(a^b)) << "\n";
+	}
+}
